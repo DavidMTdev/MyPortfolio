1ad6f4dfc10bd9bfcc32b664bb1cb365d6c61aff
David Teixeira <david.teixeira31@gmail.com>
2020-08-31 20:44:49 +0200

    add component Hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="home" class="section-hero">
+    <div class="header-hero">
+      <div class="hero-main">
+        <div class="hero-img">
+          <img src="@/assets/img/img-profile-256.jpg" alt />
+        </div>
+        <div class="hero-display">
+          <h1>
+            M. David
+            <span>Teixeira</span>
+          </h1>
+        </div>
+        <div class="hero-title">
+          <h2>Hey, Je suis Développeur</h2>
+        </div>
+        <div class="hero-subtitle">
+          <p>
+            Il semble que la perfection soit atteinte non quand il n’y a plus
+            rien à ajouter, mais quand il n’y a plus rien à retrancher.
+          </p>
+        </div>
+        <div class="hero-link">
+          <a href>
+            <svg
+              width="64"
+              height="64"
+              viewBox="0 0 64 64"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M33.5465 53.5467L48.6132 42.7734C49.8932 41.8667 50.6665 40.4 50.6665 38.8267C50.6665 34.8534 46.1865 32.56 42.9598 34.8534L31.9998 42.6667L21.0398 34.8534C17.8132 32.5334 13.3332 34.8534 13.3332 38.8267C13.3332 40.4 14.1065 41.8934 15.3865 42.8L30.4532 53.5734C31.3865 54.2134 32.6132 54.2134 33.5465 53.5467Z"
+                fill="white"
+              />
+              <path
+                d="M33.5465 34.8799L48.6132 24.1333C49.8932 23.1999 50.6665 21.7333 50.6665 20.1599C50.6665 16.1866 46.1865 13.8666 42.9598 16.1866L31.9998 23.9999L21.0398 16.1866C17.8132 13.8666 13.3332 16.1866 13.3332 20.1599C13.3332 21.7333 14.1065 23.2266 15.3865 24.1333L30.4532 34.9066C31.3865 35.5466 32.6132 35.5466 33.5465 34.8799Z"
+                fill="white"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
