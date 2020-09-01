76ad419dc4eac66c4975b57d028009e2abf3d15f
David Teixeira <david.teixeira31@gmail.com>
2020-09-01 19:13:37 +0200

    add component Contact

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <main>
     <Hero></Hero>
     <Resume></Resume>
+    <Contact></Contact>
   </main>
 </template>
 
 <script>
 import Hero from "@/components/Hero.vue";
 import Resume from "@/components/Resume.vue";
+import Contact from "@/components/Contact.vue";
 
 export default {
   name: "Home",
   components: {
     Hero,
-    Resume
+    Resume,
+    Contact
   }
 };
 </script>
