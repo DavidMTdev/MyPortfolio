fbbad823673995d6f577529da218eb716990ee5b
David Teixeira <david.teixeira31@gmail.com>
2020-09-01 17:52:31 +0200

    update component Resume => add section Experience

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -2,19 +2,24 @@
   <div id="resume" class="section-resume">
     <About></About>
     <Skills></Skills>
+    <Experience></Experience>
+    <Education></Education>
   </div>
 </template>
 
 <script>
 import About from "@/components/resume/About.vue";
 import Skills from "@/components/resume/Skills.vue";
-
+import Experience from "@/components/resume/Experience.vue";
+import Education from "@/components/resume/Education.vue";
 
 export default {
   name: "Resume",
   components: {
     About,
-    Skills
+    Skills,
+    Experience,
+    Education
   }
 };
 </script>
--- src/components/resume/Skills.vue
+++ src/components/resume/Skills.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="section-skills bg-white">
+  <div class="section-skills bg-gray">
     <div class="container pb-4">
       <div class="title">
         <div class="text-primary">
@@ -14,15 +14,15 @@
           <h5>LANGUES de programmation</h5>
         </div>
         <div class="skills-img">
-            <img src="@/assets/img/logos_html.png" alt="logo html">
-            <img src="@/assets/img/logos_css.png" alt="logo css">
-            <img src="@/assets/img/logos_javascript.png" alt="logo javascript">
-            <img src="@/assets/img/logos_python.png" alt="logo python">
-            <img src="@/assets/img/logos_c.png" alt="logo c">
-            <img src="@/assets/img/logos_java.png" alt="logo java">
-            <img src="@/assets/img/logos_php.png" alt="logo php">
-            <img src="@/assets/img/logos_sass.png" alt="logo sass">
-            <img src="@/assets/img/logos_mysql.png" alt="logo mysql">
+          <img src="@/assets/img/logos_html.png" alt="logo html" />
+          <img src="@/assets/img/logos_css.png" alt="logo css" />
+          <img src="@/assets/img/logos_javascript.png" alt="logo javascript" />
+          <img src="@/assets/img/logos_python.png" alt="logo python" />
+          <img src="@/assets/img/logos_c.png" alt="logo c" />
+          <img src="@/assets/img/logos_java.png" alt="logo java" />
+          <img src="@/assets/img/logos_php.png" alt="logo php" />
+          <img src="@/assets/img/logos_sass.png" alt="logo sass" />
+          <img src="@/assets/img/logos_mysql.png" alt="logo mysql" />
         </div>
       </div>
       <div class="main-skills">
@@ -30,11 +30,10 @@
           <h5>Frameworks / ORM</h5>
         </div>
         <div class="skills-img">
-            <img src="@/assets/img/logos_vue.png" alt="logo vue">
-            <img src="@/assets/img/logos_flask.png" alt="logo flask">
-            <img src="@/assets/img/logos_symfony.png" alt="logo symfony">
-            <img src="@/assets/img/logos_doctrine.png" alt="logo doctrine">
-
+          <img src="@/assets/img/logos_vue.png" alt="logo vue" />
+          <img src="@/assets/img/logos_flask.png" alt="logo flask" />
+          <img src="@/assets/img/logos_symfony.png" alt="logo symfony" />
+          <img src="@/assets/img/logos_doctrine.png" alt="logo doctrine" />
         </div>
       </div>
       <div class="main-skills">
@@ -42,12 +41,17 @@
           <h5>OS / OUTILS</h5>
         </div>
         <div class="skills-img">
-                <img src="@/assets/img/logos_microsoft-windows.png" alt="logo microsoft windows">
-            <img src="@/assets/img/logos_ubuntu.png" alt="logo ubuntu">
-            <img src="@/assets/img/logos_git-icon.png" alt="logo git">
-            <img src="@/assets/img/logos_visual-studio-code.png" alt="logo visual studio code">
-            <img src="@/assets/img/logos_figma.png" alt="logo figma">
-
+          <img
+            src="@/assets/img/logos_microsoft-windows.png"
+            alt="logo microsoft windows"
+          />
+          <img src="@/assets/img/logos_ubuntu.png" alt="logo ubuntu" />
+          <img src="@/assets/img/logos_git-icon.png" alt="logo git" />
+          <img
+            src="@/assets/img/logos_visual-studio-code.png"
+            alt="logo visual studio code"
+          />
+          <img src="@/assets/img/logos_figma.png" alt="logo figma" />
         </div>
       </div>
     </div>
