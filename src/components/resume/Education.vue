f48e5d8198814a70223c056a948b929f1d9fb0d5
David Teixeira <david.teixeira31@gmail.com>
2020-09-01 17:53:01 +0200

    update component Resume => add section Education

---
+++ src/components/resume/Education.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="section-education bg-gray">
+    <div class="container pb-4">
+      <div class="title">
+        <div class="text-primary">
+          <h2>EDUCATION ET DIPLOMES</h2>
+        </div>
+        <div class="text-dark">
+          <h3>Ce que j'ai fait dans ma carrière universitaire</h3>
+        </div>
+      </div>
+      <div class="main-experience">
+        <div class="grid column-2">
+          <div class="date">
+            <div class="date-text">
+              <p>2018</p>
+              <div class="arrow-right">
+                <svg width="23" height="40" viewBox="0 0 23 40" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M23 20L0.5 39.9186L0.5 0.0814152L23 20Z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div class="info pb-2 border-l-white">
+            <div class="info-content bg-white">
+              <div class="name">Baccalaureat Général Scientifique</div>
+              <div class="post">Lycée George Braques</div>
+              <div class="content">
+                Ce stage avait pour fonction principal de découvrir le monde du
+                travail ainsi que l’entreprise pour nous permettre de bien nous
+                orienter professionnellement. J’ai effectué lors de ce stage
+                plusieurs petites missions tel que l’archivage, toutefois cela a
+                pu me montrer que je ne voulais pas travailler dans ce domaine.
+              </div>
+              <div class="arrow-left fill-white">
+                <svg width="23" height="40" viewBox="0 0 23 40" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M23 20L0.5 39.9186L0.5 0.0814152L23 20Z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div class="date">
+            <div class="date-text">
+              <p>Présent</p>
+              <div class="arrow-right">
+                <svg width="23" height="40" viewBox="0 0 23 40" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M23 20L0.5 39.9186L0.5 0.0814152L23 20Z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div class="info border-l-white">
+            <div class="info-content bg-white">
+              <div class="name">Titre en cours d'acquisition</div>
+              <div class="post">Ecole Ynov Informatique</div>
+              <div class="content">
+                SFR Distributions m’a accueilli afin d’effectuer mon premier
+                stage d’étude supérieur, afin de me permettre de mettre en
+                application les connaissances acquises tout au long de l’année.
+                Durant ce stage j’ai pu découvrir le service informatique, tout
+                comme leurs projets, leur gestion, leurs problème et solutions.
+                Ce stage de six semaines m’a mis à l’épreuve en me faisant
+                réaliser quelques petites tâches et missions, tels que le
+                paramétrage et la programmation.
+              </div>
+              <div class="arrow-left fill-white">
+                <svg width="23" height="40" viewBox="0 0 23 40" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M23 20L0.5 39.9186L0.5 0.0814152L23 20Z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
