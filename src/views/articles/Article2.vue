33918872dd6555371804542d31c04298a35bb073
David Teixeira <david.teixeira31@gmail.com>
2020-09-03 18:56:08 +0200

    Add component Article => add article 2

--- src/views/articles/Article2.vue
+++ src/views/articles/Article2.vue
@@ -1,3 +1,105 @@
 <template>
-    <h1>2</h1>
+  <article class="blog-post">
+    <div class="container">
+      <div class="blog-post-header">
+        <h2 class="post-title mb-2">Rapport Stage Chez Thrill Esport Club</h2>
+        <div class="post-meta mb-3">
+          <span>
+            Aout 20, 2020 / by
+            <a href="#" target="_blank">David Teixeira</a>
+          </span>
+        </div>
+        <div class="post-banner">
+          <img class="img-fluid" src="@/assets/img/banner-thrill-esport-club.jpg" alt />
+        </div>
+      </div>
+      <div class="blog-post-body">
+        <p>
+          Thrill eSport Club est un projet concocté par un groupe de 5 amis dans
+          une perspective de se professionnaliser sur la scène du sport
+          électronique français (eSport).
+          <br />
+          <br />Les fondateurs de ce projet ont choisi de s’imposer
+          juridiquement comme une association reconnue par l’État, dans
+          l’optique de pouvoir entretenir des relations saines avec leurs futurs
+          partenaires, leurs futurs joueurs, ainsi que leur staff. Cette forme
+          juridique leurs permet dans un premier temps de créer un projet stable
+          et digne de confiance mais également de leur conférer une certaine
+          sécurité vis à vis des parties prenantes.
+        </p>
+        <h3 class="mt-2 mb-1">Analyse de la mission</h3>
+        <p>
+          Dans cette association la mission qui m’a été confié était la refonte
+          de leur site web. J’ai du donc analyser les différents documents à
+          propos du site web déjà existant, ainsi que la structure et le
+          déploiement du code source. Lors de mon analyse, sur les documents qui
+          m’ont été fourni, je découvre que le cahier des charges était
+          “minable” car il ne comportait qu'une liste du mode opératoire des
+          fonctionnalités peu détaillé du site web. De plus l’association
+          n’avait réalisé aucune charte graphique.
+        </p>
+        <h3 class="mt-2 mb-1">Rédaction des documents</h3>
+        <p>
+          Par la suite de cette analyse, j’ai du coup commencé à rédiger un
+          cahier des charges plus détaillé avec les informations apportées par
+          les fondateur de l’association pour rédiger la partie de la
+          présentation et les objectifs de lors projet. Pour la partie du mode
+          opératoire des fonctionnalité du site web, j’ai du récupérer le peu
+          d’information dont je disposais de l'ancien cahier des charges qui m'a
+          été transmise.
+        </p>
+        <div class="post-image">
+          <img class="img-fluid" src="@/assets/img/graphic-chart-home.jpg" alt />
+        </div>
+        <p>
+          Après avoir terminé de rédiger le cahier des charges, je me suis
+          tourné vers la réalisation de la charte graphique du site web, afin de
+          m’aider à réaliser les maquettes plus rapidement. J’ai commencé par
+          analyser le logotype pour rechercher et définir la palette de couleur.
+          Ensuite, je me suis mis à la recherche de deux typographie pour
+          constituer les titres et textes du site web. La partie sur les icônes
+          et les boutons n’a pas été réalisé par manque de temps et de
+          savoir-faire car “je ne suis pas designer mais développeur”.
+        </p>
+        <div class="post-image">
+          <img class="img-fluid" src="@/assets/img/graphic-chart-2x2.jpg" alt />
+        </div>
+        <p>
+          La réalisation de ce cahier des charges a amélioré mes compétences de
+          design.
+        </p>
+        <h3 class="mt-2 mb-1">Réalisation des maquettes</h3>
+        <p>
+          A la suite de la rédaction de ces documents, j’ai pu commencé à
+          réaliser les maquettes pour le nouveau site web que j’allais réaliser.
+          Par manque de compétences dans le domaine de designer, j’ai dû prendre
+          énormément de temps à réaliser ces maquettes. Toutefois, les maquettes
+          que j’ai réalisé ont été validé par l’ensemble des membres de
+          l’association.
+        </p>
+        <div class="post-image">
+          <img class="img-fluid" src="@/assets/img/new-maquette.jpg" alt />
+        </div>
+        <h3 class="mt-2 mb-1">Création de la structure du code source</h3>
+        <p>
+          Cependant, ayant consacré énormément de temps pour l’élaboration de la
+          charte graphique et des maquettes, je n’ai pu me consacrer qu’à la
+          structure du code au vu de la date de fin du stage qui approchait.
+          J’avais pour ambition de réaliser la partie Front-End avec le
+          framework VueJs et pour la partie Back-End de la faire sous forme
+          d’API REST avec la technologie Python avec son framework Flask et
+          l’ORM SQLAlchemy.
+        </p>
+        <h3 class="mt-2 mb-1">Bilan</h3>
+        <p>
+          Durant ces semaines au sein de cette association, j’ai été confronté à
+          la rédaction de documents importants pour la réalisation d’un projet
+          sans mauvaise surprise. Par ailleurs, j’ai pu me perfectionner dans le
+          domaine de Designer lors de la réalisation de maquette. De plus, étant
+          au sein d’une formation pour devenir développeur, j’ai acquéri une
+          petite compétence sur la réalisation d’une charte graphique.
+        </p>
+      </div>
+    </div>
+  </article>
 </template>
